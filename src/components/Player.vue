4191218bcfc62a0722ffcb1c4ad020174b089b6b
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-09-04 23:20:49 -0300

    Update Player.vue

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,21 +1,10 @@
 <script setup lang="ts">
 import { reactive } from 'vue';
 
-const player = reactive<IPlayer>({
-  username: "",
-  coins: 0,
-  cps: 0,
-  dps: 0,
-  idps: "",
-  shopItens: {},
-  upgrades: {},
-  skills: {}
-})
 
 </script>
 
 <template>
-        <slot name="player" :player="player"></slot>
 </template>
   
 
